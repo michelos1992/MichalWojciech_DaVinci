de0196148e19b781f77891be5ca3cee792f966b6
misiek <michel.wojciech@gmail.com>
2019-02-20 15:03:27 +0100

    Przygotowywanie projektu

--- WEBAPI/Program.cs
+++ WEBAPI/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WEBAPI
+namespace MichalWojciech_DaVinci
 {
     public class Program
     {
--- WEBAPI/Startup.cs
+++ WEBAPI/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace WEBAPI
+namespace MichalWojciech_DaVinci
 {
     public class Startup
     {
